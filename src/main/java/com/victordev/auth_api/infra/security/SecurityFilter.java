6902e734b2846726b0444ecb945f2b8eb2f559d1
João VIctor <joawrt8520@gmail.com>
2024-11-17 19:44:07 -0300

    fix: replacing userRepository with userDetailsService in SecurityFilter

--- src/main/java/com/victordev/auth_api/infra/security/SecurityFilter.java
+++ src/main/java/com/victordev/auth_api/infra/security/SecurityFilter.java
@@ -1,6 +1,5 @@
 package com.victordev.auth_api.infra.security;
 
-import com.victordev.auth_api.repository.UserRepository;
 import com.victordev.auth_api.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,14 +20,14 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
-    private final UserRepository userRepository;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if(token != null) {
             var login = tokenService.validateToken(token);
-            UserDetails user = userRepository.findByLogin(login);
+            UserDetails user = userDetailsService.loadUserByUsername(login);
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
